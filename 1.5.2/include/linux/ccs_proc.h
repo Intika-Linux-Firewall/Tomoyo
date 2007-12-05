b35e1eb311e1eb419714da85c401290ae6900b27
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2007-12-05 05:28:13 +0000

    1.5.2

---
+++ 1.5.2/include/linux/ccs_proc.h
@@ -0,0 +1,39 @@
+/*
+ * include/linux/ccs_proc.h
+ *
+ * /proc interface for SAKURA and TOMOYO.
+ *
+ * Copyright (C) 2005-2007  NTT DATA CORPORATION
+ *
+ * Version: 1.5.2   2007/12/05
+ *
+ * This file is applicable to both 2.4.30 and 2.6.11 and later.
+ * See README.ccs for ChangeLog.
+ *
+ */
+
+#ifndef _LINUX_CCS_PROC_H
+#define _LINUX_CCS_PROC_H
+
+#ifndef __user
+#define __user
+#endif
+
+/*************************  Indexes for /proc interfaces.  *************************/
+
+#define CCS_DOMAINPOLICY          0
+#define CCS_EXCEPTIONPOLICY       1
+#define CCS_SYSTEMPOLICY          2
+#define CCS_DOMAIN_STATUS         3
+#define CCS_PROCESS_STATUS        4
+#define CCS_MEMINFO               5
+#define CCS_GRANTLOG              6
+#define CCS_REJECTLOG             7
+#define CCS_SELFDOMAIN            8
+#define CCS_VERSION               9
+#define CCS_PROFILE              10
+#define CCS_QUERY                11
+#define CCS_MANAGER              12
+#define CCS_UPDATESCOUNTER       13
+
+#endif
