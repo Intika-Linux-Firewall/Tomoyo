6d6eff6e26d47f9ddb9b0a81163d05107059a3e4
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2008-06-05 03:58:49 +0000

    Fix realpath for pipes and sockets

--- trunk/1.6.x/ccs-patch/fs/ccs_common.c
+++ trunk/1.6.x/ccs-patch/fs/ccs_common.c
@@ -5,7 +5,7 @@
  *
  * Copyright (C) 2005-2008  NTT DATA CORPORATION
  *
- * Version: 1.6.1   2008/06/04
+ * Version: 1.6.1   2008/06/05
  *
  * This file is applicable to both 2.4.30 and 2.6.11 and later.
  * See README.ccs for ChangeLog.
@@ -2388,10 +2388,10 @@ void ccs_load_policy(const char *filename)
 	}
 #endif
 #ifdef CONFIG_SAKURA
-	printk(KERN_INFO "SAKURA: 1.6.1   2008/06/04\n");
+	printk(KERN_INFO "SAKURA: 1.6.1   2008/06/05\n");
 #endif
 #ifdef CONFIG_TOMOYO
-	printk(KERN_INFO "TOMOYO: 1.6.1   2008/06/04\n");
+	printk(KERN_INFO "TOMOYO: 1.6.1   2008/06/05\n");
 #endif
 	printk(KERN_INFO "Mandatory Access Control activated.\n");
 	sbin_init_started = true;
