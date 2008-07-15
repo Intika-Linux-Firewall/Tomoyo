96b7bec3675fd07440f053fad9bca3256af6cdb2
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2008-07-15 05:17:52 +0000

    1.6.3

---
+++ tags/ccs-patch/1.6.3/specs/build-c4-2.6.9.sh
@@ -0,0 +1,88 @@
+#! /bin/sh
+#
+# This is a kernel build script for CentOS 4.6's 2.6.9 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-2.6.9-67.0.20.EL.src.rpm ]
+then
+    wget http://ftp.riken.jp/Linux/centos/4.6/updates/SRPMS/kernel-2.6.9-67.0.20.EL.src.rpm || die "Can't download source package."
+fi
+rpm -ivh kernel-2.6.9-67.0.20.EL.src.rpm || die "Can't install source package."
+
+cd /usr/src/redhat/SOURCES/ || die "Can't chdir to /usr/src/redhat/SOURCES/ ."
+if [ ! -r ccs-patch-1.6.3-20080715.tar.gz ]
+then
+    wget http://osdn.dl.sourceforge.jp/tomoyo/30297/ccs-patch-1.6.3-20080715.tar.gz || die "Can't download patch."
+fi
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+cp -p /usr/src/redhat/SPECS/kernel-2.6.spec . || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- kernel-2.6.spec	2008-06-26 20:41:46.000000000 +0900
++++ kernel-2.6.spec	2008-06-28 12:27:49.000000000 +0900
+@@ -26,7 +26,7 @@
+ # that the kernel isn't the stock distribution kernel, for example by
+ # adding some text to the end of the version number.
+ #
+-%define release 67.0.20.EL
++%define release 67.0.20.EL_tomoyo_1.6.3
+ %define sublevel 9
+ %define kversion 2.6.%{sublevel}
+ %define rpmversion 2.6.%{sublevel}
+@@ -132,6 +132,9 @@
+ # to versions below the minimum
+ #
+ 
++# TOMOYO Linux
++%define signmodules 0
++
+ #
+ # First the general kernel 2.6 required versions as per
+ # Documentation/Changes
+@@ -168,7 +171,7 @@
+ %define __find_provides /usr/lib/rpm/redhat/find-kmod-provides.sh
+ %define __find_requires %{nil}
+ 
+-Name: kernel
++Name: ccs-kernel
+ Group: System Environment/Kernel
+ License: GPLv2
+ Version: %{rpmversion}
+@@ -3788,6 +3791,11 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.6.3-20080715.tar.gz
++sed -i -e "s/^EXTRAVERSION.*/EXTRAVERSION = -67.0.20.EL/" -- Makefile
++patch -sp1 < patches/ccs-patch-2.6.9-67.0.20.EL.diff
++
+ cp %{SOURCE10} Documentation/
+ 
+ mkdir configs
+@@ -3799,6 +3807,9 @@
+ for i in *.config 
+ do 
+ 	mv $i .config 
++	# TOMOYO Linux
++	cat config.ccs >> .config
++	sed -i -e "s/^CONFIG_DEBUG_INFO=.*/# CONFIG_DEBUG_INFO is not set/" -- .config
+ 	make ARCH=`echo $i | cut -d"-" -f3 | cut -d"." -f1 | sed -e s/i.86/i386/ -e s/s390x/s390/ -e s/ppc64.series/ppc64/  ` nonint_oldconfig > /dev/null 
+ 	cp .config configs/$i 
+ done
+EOF
+mv kernel-2.6.spec kernel-2.6.9-67.0.20.EL_tomoyo_1.6.3.spec || die "Can't rename spec file."
+echo ""
+echo ""
+echo ""
+echo "Edit /tmp/kernel-2.6.9-67.0.20.EL_tomoyo_1.6.3.spec if needed, and run"
+echo "rpmbuild -bb /tmp/kernel-2.6.9-67.0.20.EL_tomoyo_1.6.3.spec"
+echo "to build kernel rpm packages."
+exit 0
---
+++ tags/ccs-patch/1.6.3/specs/build-vl4.2-2.6.16.sh
@@ -0,0 +1,88 @@
+#! /bin/sh
+#
+# This is a kernel build script for VineLinux 4.2's 2.6.16 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-2.6.16-76.37vl4.src.rpm ]
+then
+    wget http://updates.vinelinux.org/Vine-4.2/updates/SRPMS/kernel-2.6.16-76.37vl4.src.rpm || die "Can't download source package."
+fi
+rpm -ivh kernel-2.6.16-76.37vl4.src.rpm || die "Can't install source package."
+
+cd /usr/src/vine/SOURCES/ || die "Can't chdir to /usr/src/vine/SOURCES/ ."
+if [ ! -r ccs-patch-1.6.3-20080715.tar.gz ]
+then
+    wget http://osdn.dl.sourceforge.jp/tomoyo/30297/ccs-patch-1.6.3-20080715.tar.gz || die "Can't download patch."
+fi
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+cp -p /usr/src/vine/SPECS/kernel-2.6-vl.spec . || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- kernel-2.6-vl.spec	2008-06-11 17:38:52.000000000 +0900
++++ kernel-2.6-vl.spec	2008-06-26 13:16:35.000000000 +0900
+@@ -23,7 +23,7 @@
+ %define sublevel 16
+ %define kversion 2.6.%{sublevel}
+ %define rpmversion 2.6.%{sublevel}
+-%define release 76.37%{_dist_release}
++%define release 76.37%{_dist_release}_tomoyo_1.6.3
+ 
+ %define make_target bzImage
+ 
+@@ -100,6 +100,9 @@
+ # to versions below the minimum
+ #
+ 
++# TOMOYO Linux
++%define signmodules 0
++
+ #
+ # First the general kernel 2.6 required versions as per
+ # Documentation/Changes
+@@ -126,7 +129,7 @@
+ #
+ %define kernel_prereq  fileutils, modutils >= 3.2.2 , initscripts >= 5.83, mkinitrd >= 4.2.1.8-0vl2.1
+ 
+-Name: kernel
++Name: ccs-kernel
+ Group: System Environment/Kernel
+ License: GPLv2
+ Version: %{rpmversion}
+@@ -1072,6 +1075,11 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.6.3-20080715.tar.gz
++sed -i -e "s/^EXTRAVERSION.*/EXTRAVERSION = -76.37vl4custom/" -- Makefile
++patch -sp1 < patches/ccs-patch-2.6.16-76.37vl4.diff
++
+ cp %{SOURCE10} Documentation/
+ 
+ # put Vine logo
+@@ -1092,6 +1100,9 @@
+ for i in *.config
+ do 
+ 	mv $i .config 
++	# TOMOYO Linux
++	cat config.ccs >> .config
++	sed -i -e "s/^CONFIG_DEBUG_INFO=.*/# CONFIG_DEBUG_INFO is not set/" -- .config
+ 	Arch=`head -1 .config | cut -b 3-`
+ 	echo "# $Arch" > configs/$i
+ 	cat .config >> configs/$i 
+EOF
+mv kernel-2.6-vl.spec kernel-2.6.16-76.37vl4_tomoyo_1.6.3.spec || die "Can't rename spec file."
+echo ""
+echo ""
+echo ""
+echo "Edit /tmp/kernel-2.6.16-76.37vl4_tomoyo_1.6.3.spec if needed, and run"
+echo "rpmbuild -bb /tmp/kernel-2.6.16-76.37vl4_tomoyo_1.6.3.spec"
+echo "to build kernel rpm packages."
+exit 0
