c3bd084d1eb60f00963326343a8be714b07ad9e3
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2008-11-11 01:34:56 +0000

    1.6.5

---
+++ 1.6.5/specs/build-debian_sarge-2.4.27.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+#
+# This is kernel build script for debian sarge's 2.4.27 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+export CONCURRENCY_LEVEL=`grep -c '^processor' /proc/cpuinfo` || die "Can't export."
+
+# Download TOMOYO Linux patches.
+mkdir -p /usr/src/rpm/SOURCES/
+cd /usr/src/rpm/SOURCES/ || die "Can't chdir to /usr/src/rpm/SOURCES/ ."
+if [ ! -r ccs-patch-1.6.5-20081111.tar.gz ]
+then
+    wget http://osdn.dl.sourceforge.jp/tomoyo/30297/ccs-patch-1.6.5-20081111.tar.gz || die "Can't download patch."
+fi
+
+# Install kernel source packages.
+cd /usr/src/ || die "Can't chdir to /usr/src/ ."
+apt-get install fakeroot build-essential || die "Can't install packages."
+apt-get install kernel-source-2.4.27 || die "Can't install packages."
+apt-get install kernel-patch-debian-2.4.27 || die "Can't install packages."
+apt-get build-dep kernel-image-2.4.27-4-686-smp || die "Can't install packages."
+apt-get source kernel-image-2.4.27-4-686-smp || die "Can't install kernel source."
+
+# Apply patches and create kernel config.
+cd kernel-image-2.4.27-i386-2.4.27/ || die "Can't chdir to kernel-image-2.4.27-i386-2.4.27/ ."
+tar -zxf /usr/src/rpm/SOURCES/ccs-patch-1.6.5-20081111.tar.gz config.ccs || die "Can't extract patch."
+cat config/686-smp config.ccs > config/686-smp-ccs || die "Can't create config."
+debian/rules flavours=686-smp-ccs || die "Can't run rules."
+cd build-686-smp-ccs/ || die "Can't chdir to build-686-smp-ccs/ ."
+tar -zxf /usr/src/rpm/SOURCES/ccs-patch-1.6.5-20081111.tar.gz || die "Can't extract patch."
+patch -p1 < patches/ccs-patch-2.4.27-debian-sarge.diff || die "Can't apply patch."
+awk ' BEGIN { flag = 0; print ""; } { if ( $1 == "Package:") { if ( index($2, "-686-smp") > 0) flag = 1; else flag = 0; }; if (flag) print $0; } ' debian/control | sed -e 's:-686-smp:-686-smp-ccs:g' > debian/control.tmp || die "Can't create file."
+cat debian/control.tmp >> debian/control || die "Can't edit file."
+cd .. || die "Can't chdir to ../ ."
+
+# Start compilation.
+debian/rules binary-arch flavours=686-smp-ccs || die "Failed to build kernel package."
+
+exit 0
---
+++ 1.6.5/specs/build-f9-2.6.26.sh
@@ -0,0 +1,98 @@
+#! /bin/sh
+#
+# This is a kernel build script for Fedora 9's 2.6.26 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-2.6.26.6-79.fc9.src.rpm ]
+then
+    wget http://ftp.riken.jp/Linux/fedora/updates/9/SRPMS.newkey/kernel-2.6.26.6-79.fc9.src.rpm || die "Can't download source package."
+fi
+rpm -ivh kernel-2.6.26.6-79.fc9.src.rpm || die "Can't install source package."
+
+cd /usr/src/redhat/SOURCES/ || die "Can't chdir to /usr/src/redhat/SOURCES/ ."
+if [ ! -r ccs-patch-1.6.5-20081111.tar.gz ]
+then
+    wget http://osdn.dl.sourceforge.jp/tomoyo/30297/ccs-patch-1.6.5-20081111.tar.gz || die "Can't download patch."
+fi
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+cp -p /usr/src/redhat/SPECS/kernel.spec . || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- kernel.spec	2008-10-18 03:00:07.000000000 +0900
++++ kernel.spec	2008-10-21 15:21:52.000000000 +0900
+@@ -12,7 +12,7 @@
+ # that the kernel isn't the stock distribution kernel, for example,
+ # by setting the define to ".local" or ".bz123456"
+ #
+-#% define buildid .local
++%define buildid _tomoyo_1.6.5
+ 
+ # fedora_build defines which build revision of this kernel version we're
+ # building. Rather than incrementing forever, as with the prior versioning
+@@ -400,6 +400,11 @@
+ # to versions below the minimum
+ #
+ 
++# TOMOYO Linux
++%define with_modsign 0
++%define _enable_debug_packages 0
++%define with_debuginfo 0
++
+ #
+ # First the general kernel 2.6 required versions as per
+ # Documentation/Changes
+@@ -456,7 +461,7 @@
+ AutoProv: yes\
+ %{nil}
+ 
+-Name: kernel%{?variant}
++Name: ccs-kernel%{?variant}
+ Group: System Environment/Kernel
+ License: GPLv2
+ URL: http://www.kernel.org/
+@@ -831,7 +836,7 @@
+ Provides: kernel-devel-uname-r = %{KVERREL}%{?1:.%{1}}\
+ AutoReqProv: no\
+ Requires(pre): /usr/bin/find\
+-%description -n kernel%{?variant}%{?1:-%{1}}-devel\
++%description -n ccs-kernel%{?variant}%{?1:-%{1}}-devel\
+ This package provides kernel headers and makefiles sufficient to build modules\
+ against the %{?2:%{2} }kernel package.\
+ %{nil}
+@@ -1367,6 +1372,10 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.6.5-20081111.tar.gz
++patch -sp1 < patches/ccs-patch-2.6.26-fedora-9.diff
++
+ %endif
+ 
+ # Any further pre-build tree manipulations happen here.
+@@ -1395,6 +1404,9 @@
+ for i in *.config
+ do
+   mv $i .config
++  # TOMOYO Linux
++  cat config.ccs >> .config
++  sed -i -e 's:CONFIG_DEBUG_INFO=.*:# CONFIG_DEBUG_INFO is not set:' -- .config
+   Arch=`head -1 .config | cut -b 3-`
+   make ARCH=$Arch %{oldconfig_target} > /dev/null
+   echo "# $Arch" > configs/$i
+EOF
+mv kernel.spec ccs-kernel.spec || die "Can't rename spec file."
+echo ""
+echo ""
+echo ""
+echo "Edit /tmp/ccs-kernel.spec if needed, and run"
+echo "rpmbuild -bb /tmp/ccs-kernel.spec"
+echo "to build kernel rpm packages."
+exit 0
