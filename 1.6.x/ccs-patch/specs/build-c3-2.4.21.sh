849233117f482f897ee216e137ee10334203af5b
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2009-02-02 03:47:38 +0000

    1.6.6

--- 1.6.x/ccs-patch/specs/build-c3-2.4.21.sh
+++ 1.6.x/ccs-patch/specs/build-c3-2.4.21.sh
@@ -17,9 +17,9 @@ fi
 rpm -ivh kernel-2.4.21-58.EL.src.rpm || die "Can't install source package."
 
 cd /usr/src/redhat/SOURCES/ || die "Can't chdir to /usr/src/redhat/SOURCES/ ."
-if [ ! -r ccs-patch-1.6.5-20081225.tar.gz ]
+if [ ! -r ccs-patch-1.6.6-20090202.tar.gz ]
 then
-    wget http://osdn.dl.sourceforge.jp/tomoyo/30297/ccs-patch-1.6.5-20081225.tar.gz || die "Can't download patch."
+    wget http://osdn.dl.sourceforge.jp/tomoyo/30297/ccs-patch-1.6.6-20090202.tar.gz || die "Can't download patch."
 fi
 
 cd /tmp/ || die "Can't chdir to /tmp/ ."
@@ -32,7 +32,7 @@ patch << "EOF" || die "Can't patch spec file."
  # adding some text to the end of the version number.
  #
 -%define release 58.EL
-+%define release 58.EL_tomoyo_1.6.5
++%define release 58.EL_tomoyo_1.6.6
  %define sublevel 21
  %define kversion 2.4.%{sublevel}
  # /usr/src/%{kslnk} -> /usr/src/linux-%{KVERREL}
@@ -50,7 +50,7 @@ patch << "EOF" || die "Can't patch spec file."
  # END OF PATCH APPLICATIONS
  
 +# TOMOYO Linux
-+tar -zxf %_sourcedir/ccs-patch-1.6.5-20081225.tar.gz
++tar -zxf %_sourcedir/ccs-patch-1.6.6-20090202.tar.gz
 +patch -sp1 < patches/ccs-patch-2.4.21-centos-3.9.diff
 +
  cp %{SOURCE10} Documentation/
