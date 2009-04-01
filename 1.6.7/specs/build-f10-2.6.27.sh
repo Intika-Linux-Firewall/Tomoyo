1d57f4db71f4ccf4b1dbeeaecb344bae5a5889f5
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2009-04-01 07:39:37 +0000

    1.6.7

---
+++ 1.6.7/specs/build-f10-2.6.27.sh
@@ -0,0 +1,107 @@
+#! /bin/sh
+#
+# This is a kernel build script for Fedora 10's 2.6.27 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-2.6.27.19-170.2.35.fc10.src.rpm ]
+then
+    wget http://ftp.riken.jp/Linux/fedora/development/source/SRPMS/kernel-2.6.27.19-170.2.35.fc10.src.rpm || die "Can't download source package."
+fi
+rpm -ivh kernel-2.6.27.19-170.2.35.fc10.src.rpm || die "Can't install source package."
+
+cd /root/rpmbuild/SOURCES/ || die "Can't chdir to /root/rpmbuild/SOURCES/ ."
+if [ ! -r ccs-patch-1.6.7-20090401.tar.gz ]
+then
+    wget http://osdn.dl.sourceforge.jp/tomoyo/30297/ccs-patch-1.6.7-20090401.tar.gz || die "Can't download patch."
+fi
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+cp -p /root/rpmbuild/SPECS/kernel.spec . || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- kernel.spec	2009-02-24 02:38:31.000000000 +0900
++++ kernel.spec	2009-03-03 09:41:31.000000000 +0900
+@@ -12,7 +12,7 @@
+ # that the kernel isn't the stock distribution kernel, for example,
+ # by setting the define to ".local" or ".bz123456"
+ #
+-#% define buildid .local
++%define buildid _tomoyo_1.6.7
+ 
+ # fedora_build defines which build revision of this kernel version we're
+ # building. Rather than incrementing forever, as with the prior versioning
+@@ -384,6 +384,11 @@
+ # to versions below the minimum
+ #
+ 
++# TOMOYO Linux
++%define with_modsign 0
++%define _enable_debug_packages 0
++%define with_debuginfo 0
++
+ #
+ # First the general kernel 2.6 required versions as per
+ # Documentation/Changes
+@@ -422,7 +427,7 @@
+ # Packages that need to be installed before the kernel is, because the %post
+ # scripts use them.
+ #
+-%define kernel_prereq  fileutils, module-init-tools, initscripts >= 8.11.1-1, mkinitrd >= 6.0.61-1, kernel-firmware >= %{rpmversion}-%{pkg_release}
++%define kernel_prereq  fileutils, module-init-tools, initscripts >= 8.11.1-1, mkinitrd >= 6.0.61-1
+ 
+ #
+ # This macro does requires, provides, conflicts, obsoletes for a kernel package.
+@@ -450,7 +455,7 @@
+ AutoProv: yes\
+ %{nil}
+ 
+-Name: kernel%{?variant}
++Name: ccs-kernel%{?variant}
+ Group: System Environment/Kernel
+ License: GPLv2
+ URL: http://www.kernel.org/
+@@ -864,7 +869,7 @@
+ Provides: kernel-devel-uname-r = %{KVERREL}%{?1:.%{1}}\
+ AutoReqProv: no\
+ Requires(pre): /usr/bin/find\
+-%description -n kernel%{?variant}%{?1:-%{1}}-devel\
++%description -n ccs-kernel%{?variant}%{?1:-%{1}}-devel\
+ This package provides kernel headers and makefiles sufficient to build modules\
+ against the %{?2:%{2} }kernel package.\
+ %{nil}
+@@ -1390,6 +1395,10 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.6.7-20090401.tar.gz
++patch -sp1 < patches/ccs-patch-2.6.27-fedora-10.diff
++
+ %endif
+ 
+ # Any further pre-build tree manipulations happen here.
+@@ -1416,6 +1425,9 @@
+ for i in *.config
+ do
+   mv $i .config
++  # TOMOYO Linux
++  cat config.ccs >> .config
++  sed -i -e 's:CONFIG_DEBUG_INFO=.*:# CONFIG_DEBUG_INFO is not set:' -- .config
+   Arch=`head -1 .config | cut -b 3-`
+   make ARCH=$Arch %{oldconfig_target} > /dev/null
+   echo "# $Arch" > configs/$i
+EOF
+mv kernel.spec ccs-kernel.spec || die "Can't rename spec file."
+echo ""
+echo ""
+echo ""
+echo "Edit /tmp/ccs-kernel.spec if needed, and run"
+echo "rpmbuild -bb /tmp/ccs-kernel.spec"
+echo "to build kernel rpm packages."
+exit 0
