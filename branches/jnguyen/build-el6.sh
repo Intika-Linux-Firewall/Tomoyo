78a909ff2af36e64603bbeae2fd04ac62f1ec328
jnguyen <jnguyen@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2011-11-05 21:14:02 +0000

    update buildscripts

---
+++ branches/jnguyen/build-el6.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+. "${RPM_SCRIPT_DIR}/global_functions"
+
+URL_CCS="http://sourceforge.jp/frs/redir.php?f=/tomoyo/49684"
+URL_CCS_SVN="http://sourceforge.jp/projects/tomoyo/svn/view/trunk/1.8.x/ccs-patch/patches"
+URL_KERNEL="http://www.mirrorservice.org/sites/ftp.scientificlinux.org/linux/scientific/6.1/SRPMS/vendor"
+
+ARCH="$(uname -m)"
+CCS_VER="1.8.3p1"
+CCSPATCH_VER="1.8.3-20111025"
+KERNEL_VER="2.6.32-131.17.1.el6"
+
+UPDATED_DIFF=0
+CCSDIFF_NAME="ccs-patch-2.6.32-centos-6.1.diff"
+#CCSDIFF_REVISION=""
+
+setup_rpmbuild_tree
+setup_source_dir "${RPM_SOURCE_DIR}/ccs-kernel-${KERNEL_VER}"
+cd "${RPM_SOURCE_DIR}/ccs-kernel-${KERNEL_VER}"
+
+download_file "${URL_CCS}/ccs-patch-${CCSPATCH_VER}.tar.gz" \
+	"ccs-patch-${CCSPATCH_VER}.tar.gz"
+if [[ "${UPDATED_DIFF}" = 1 ]]; then
+	download_file "${URL_CCS_SVN}/${CCSDIFF_REVISION}" "${CCSDIFF_NAME}"
+fi
+download_file "${URL_KERNEL}/kernel-${KERNEL_VER}.src.rpm" \
+	"kernel-${KERNEL_VER}.src.rpm"
+
+install_srpm "kernel-${KERNEL_VER}.src.rpm"
+
+cd "${RPM_BUILD_DIR}/SPECS"
+cp -v "kernel.spec" "ccs-kernel.spec"
+
+# Before applying the patch, replace the placeholder variables with the real values.
+msg "Patching ccs-kernel.spec ..."
+if [[ "${UPDATED_DIFF}" = 0 ]]; then
+	sed -e "s#\${CCS_VER}#${CCS_VER}#g" \
+		-e "s#\${CCSPATCH_VER}#${CCSPATCH_VER}#g" \
+		-e "s#patches/\${CCSDIFF_NAME}#patches/${CCSDIFF_NAME}#g" \
+		"${RPM_SCRIPT_DIR}/${0%.*}.patch" | patch
+elif [[ "${UPDATED_DIFF}" = 1 ]]; then
+	sed -e "s#\${CCS_VER}#${CCS_VER}#g" \
+		-e "s#\${CCSPATCH_VER}#${CCSPATCH_VER}#g" \
+		-e "s#patches/\${CCSDIFF_NAME}#%_sourcedir/${CCSDIFF_NAME}#g" \
+		"${RPM_SCRIPT_DIR}/${0%.*}.patch" | patch
+fi
+[[ $? != 0 ]] && error "ERROR: patching ccs-kernel.spec failed"
+
+rpmbuild -bs "${RPM_BUILD_DIR}/SPECS/ccs-kernel.spec"
--- branches/jnguyen/build-tools.sh
+++ branches/jnguyen/build-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. ./global_functions
+. "${RPM_SCRIPT_DIR}/global_functions"
 
 URL_CCS="http://sourceforge.jp/frs/redir.php?f=/tomoyo/49693"
 
@@ -8,7 +8,8 @@ ARCH="$(uname -m)"
 CCSTOOLS_VER="1.8.3-20111025"
 
 setup_rpmbuild_tree
-enter_dir "${SOURCE_DIR}"
+setup_source_dir "${RPM_SOURCE_DIR}/ccs-tools-${CCSTOOLS_VER}"
+cd "${RPM_SOURCE_DIR}/ccs-tools-${CCSTOOLS_VER}"
 
 download_file "${URL_CCS}/ccs-tools-${CCSTOOLS_VER}.tar.gz" \
 	"ccs-tools-${CCSTOOLS_VER}.tar.gz"
@@ -17,7 +18,8 @@ msg "Extracting ccs-tools.spec ..."
 tar -zxvf "ccs-tools-${CCSTOOLS_VER}.tar.gz" ccstools/ccs-tools.spec -O \
 	> "${RPM_BUILD_DIR}/SPECS/ccs-tools.spec"
 
-run_build --target ${ARCH} --with baseonly --without debug \
-	--without debuginfo "${RPM_BUILD_DIR}/SPECS/ccs-tools.spec"
+cd "${RPM_BUILD_DIR}/SPECS"
 
-exit 0
+patch < "${RPM_SCRIPT_DIR}/${0%.*}.patch"
+
+rpmbuild -bs "${RPM_BUILD_DIR}/SPECS/ccs-tools.spec"
