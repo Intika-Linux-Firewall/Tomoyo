a48dbc3d065c70e6645a003288b36eca3bcbfb37
jnguyen <jnguyen@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2012-07-20 09:36:15 +0000

    update buildscripts

--- branches/jamielinux/buildscripts/build-el6.sh
+++ branches/jamielinux/buildscripts/build-el6.sh
@@ -10,7 +10,7 @@ URL_KERNEL="ftp://ftp.redhat.com/pub/redhat/linux/enterprise/6Server/en/os/SRPMS
 ARCH="$(uname -m)"
 CCS_VER="1.8.3p7.2"
 CCS_PATCH_VER="1.8.3-20120710"
-KERNEL_VER="2.6.32-279.1.1.el6"
+KERNEL_VER="2.6.32-279.2.1.el6"
 
 CCS_DIFF_NAME="ccs-patch-2.6.32-centos-6.3.diff"
 
