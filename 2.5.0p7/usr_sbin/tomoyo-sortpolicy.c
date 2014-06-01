435a27fef12312050bbeef633c2d4c47ef6a4584
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2014-06-01 03:57:11 +0000

    ccs-tools-1.8.3p8 tomoyo-tools-2.4.0p6 tomoyo-tools-2.5.0p7

---
+++ 2.5.0p7/usr_sbin/tomoyo-sortpolicy.c
@@ -0,0 +1,32 @@
+/*
+ * tomoyo-sortpolicy.c
+ *
+ * TOMOYO Linux's utilities.
+ *
+ * Copyright (C) 2005-2011  NTT DATA CORPORATION
+ *
+ * Version: 2.5.0   2011/09/29
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License v2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+#include "tomoyotools.h"
+
+int main(int argc, char *argv[])
+{
+	struct ccs_domain_policy dp = { NULL, 0, NULL };
+	ccs_read_domain_policy(&dp, NULL);
+	ccs_write_domain_policy(&dp, 1);
+	ccs_clear_domain_policy(&dp);
+	return 0;
+}
