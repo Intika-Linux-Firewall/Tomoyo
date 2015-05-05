58e70ca12531c8af0f2a054dea4513c55967b34b
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2015-05-05 13:10:39 +0000

    ccs-patch 1.8.4 / ccs-tools 1.8.4

---
+++ 1.8.4/specs/build-f19-3.14.sh
@@ -0,0 +1,110 @@
+#! /bin/sh
+#
+# This is a kernel build script for Fedora 19's 3.14 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+yum -y install tar wget rpm-build make gcc patch redhat-rpm-config xmlto asciidoc gnupg elfutils-devel zlib-devel binutils-devel newt-devel python-devel perl-ExtUtils-Embed pciutils-devel hmaccalc bison net-tools audit-libs-devel pesign bc ncurses-devel
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-3.14.27-100.fc19.src.rpm ]
+then
+    wget https://archives.fedoraproject.org/pub/archive/fedora/linux/updates/19/SRPMS/kernel-3.14.27-100.fc19.src.rpm || die "Can't download source package."
+fi
+LANG=C rpm --checksig kernel-3.14.27-100.fc19.src.rpm | grep -F ': rsa sha1 (md5) pgp md5 OK' || die "Can't verify signature."
+rpm -ivh kernel-3.14.27-100.fc19.src.rpm || die "Can't install source package."
+
+cd /root/rpmbuild/SOURCES/ || die "Can't chdir to /root/rpmbuild/SOURCES/ ."
+if [ ! -r ccs-patch-1.8.4-20150505.tar.gz ]
+then
+    wget -O ccs-patch-1.8.4-20150505.tar.gz 'http://sourceforge.jp/frs/redir.php?f=/tomoyo/49684/ccs-patch-1.8.4-20150505.tar.gz' || die "Can't download patch."
+fi
+
+cd /root/rpmbuild/SPECS/ || die "Can't chdir to /root/rpmbuild/SPECS/ ."
+cp -p kernel.spec ccs-kernel.spec || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- ccs-kernel.spec
++++ ccs-kernel.spec
+@@ -31,7 +31,7 @@
+ #
+ # (Uncomment the '#' and both spaces below to set the buildid.)
+ #
+-# % define buildid .local
++%define buildid _tomoyo_1.8.4
+ ###################################################################
+ 
+ # The buildid can also be specified on the rpmbuild command line
+@@ -490,7 +490,7 @@
+ AutoProv: yes\
+ %{nil}
+ 
+-Name: kernel%{?variant}
++Name: ccs-kernel%{?variant}
+ Group: System Environment/Kernel
+ License: GPLv2 and Redistributable, no modification permitted
+ URL: http://www.kernel.org/
+@@ -945,7 +945,7 @@
+ AutoReqProv: no\
+ Requires(pre): /usr/bin/find\
+ Requires: perl\
+-%description -n kernel%{?variant}%{?1:-%{1}}-devel\
++%description -n ccs-kernel%{?variant}%{?1:-%{1}}-devel\
+ This package provides kernel headers and makefiles sufficient to build modules\
+ against the %{?2:%{2} }kernel package.\
+ %{nil}
+@@ -965,7 +965,7 @@
+ Provides: kernel%{?1:-%{1}}-modules-extra-uname-r = %{KVERREL}%{?1:.%{1}}\
+ Requires: kernel-uname-r = %{KVERREL}%{?1:.%{1}}\
+ AutoReqProv: no\
+-%description -n kernel%{?variant}%{?1:-%{1}}-modules-extra\
++%description -n ccs-kernel%{?variant}%{?1:-%{1}}-modules-extra\
+ This package provides less commonly used kernel modules for the %{?2:%{2} }kernel package.\
+ %{nil}
+ 
+@@ -1462,6 +1462,10 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.8.4-20150505.tar.gz
++patch -sp1 < patches/ccs-patch-3.14-fedora-19.diff
++
+ %endif
+ 
+ # Any further pre-build tree manipulations happen here.
+@@ -1484,6 +1488,17 @@
+ for i in *.config
+ do
+   mv $i .config
++  # TOMOYO Linux 2.5
++  sed -i -e 's/# CONFIG_SECURITY_PATH is not set/CONFIG_SECURITY_PATH=y/' -- .config
++  sed -i -e 's/# CONFIG_SECURITY_TOMOYO is not set/CONFIG_SECURITY_TOMOYO=y/' -- .config
++  echo 'CONFIG_SECURITY_TOMOYO_MAX_ACCEPT_ENTRY=2048' >> .config
++  echo 'CONFIG_SECURITY_TOMOYO_MAX_AUDIT_LOG=1024' >> .config
++  echo '# CONFIG_SECURITY_TOMOYO_OMIT_USERSPACE_LOADER is not set' >> .config
++  echo 'CONFIG_SECURITY_TOMOYO_POLICY_LOADER="/sbin/tomoyo-init"' >> .config
++  echo 'CONFIG_SECURITY_TOMOYO_ACTIVATION_TRIGGER="/sbin/init"' >> .config
++  echo '# CONFIG_DEFAULT_SECURITY_TOMOYO is not set' >> .config
++  # TOMOYO Linux 1.8
++  cat config.ccs >> .config
+   Arch=`head -1 .config | cut -b 3-`
+   make ARCH=$Arch listnewconfig | grep -E '^CONFIG_' >.newoptions || true
+ %if %{listnewconfig_fail}
+EOF
+echo ""
+echo ""
+echo ""
+echo "Edit /root/rpmbuild/SPECS/ccs-kernel.spec if needed, and run"
+echo "rpmbuild -bb /root/rpmbuild/SPECS/ccs-kernel.spec"
+echo "to build kernel rpm packages."
+echo ""
+ARCH=`uname -m`
+echo "I'll start 'rpmbuild -bb --target $ARCH --without debug --without debuginfo /root/rpmbuild/SPECS/ccs-kernel.spec' in 30 seconds. Press Ctrl-C to stop."
+sleep 30
+exec rpmbuild -bb --target $ARCH --without debug --without debuginfo /root/rpmbuild/SPECS/ccs-kernel.spec
+exit 0
---
+++ 1.8.4/specs/build-vl6-3.4.sh
@@ -0,0 +1,93 @@
+#! /bin/sh
+#
+# This is a kernel build script for VineLinux 6.2's 3.4 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-3.4.106-1vl6.src.rpm ]
+then
+    wget http://updates.vinelinux.org/Vine-6.3/updates/SRPMS/kernel-3.4.106-1vl6.src.rpm || die "Can't download source package."
+fi
+LANG=C rpm --checksig kernel-3.4.106-1vl6.src.rpm | grep -F ': (sha1) dsa sha1 md5 gpg OK' || die "Can't verify signature."
+rpm -ivh kernel-3.4.106-1vl6.src.rpm || die "Can't install source package."
+
+cd /root/rpm/SOURCES/ || die "Can't chdir to /root/rpm/SOURCES/ ."
+if [ ! -r ccs-patch-1.8.4-20150505.tar.gz ]
+then
+    wget -O ccs-patch-1.8.4-20150505.tar.gz 'http://sourceforge.jp/frs/redir.php?f=/tomoyo/49684/ccs-patch-1.8.4-20150505.tar.gz' || die "Can't download patch."
+fi
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+cp -p /root/rpm/SPECS/kernel34-vl.spec . || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- kernel34-vl.spec
++++ kernel34-vl.spec
+@@ -34,7 +34,7 @@
+ %define patchlevel 106
+ %define kversion 3.%{sublevel}
+ %define rpmversion 3.%{sublevel}.%{patchlevel}
+-%define release 1%{?_dist_release}
++%define release 1%{?_dist_release}_tomoyo_1.8.4
+ 
+ %define make_target bzImage
+ %define hdrarch %_target_cpu
+@@ -126,6 +126,9 @@
+ # to versions below the minimum
+ #
+ 
++# TOMOYO Linux
++%define signmodules 0
++
+ #
+ # First the general kernel 2.6 required versions as per
+ # Documentation/Changes
+@@ -158,7 +161,7 @@
+ #
+ %define kernel_prereq  fileutils, %{kmod}, initscripts >= 8.80, mkinitrd >= 6.0.93, linux-firmware >= 20110601-1
+ 
+-Name: kernel
++Name: ccs-kernel
+ Group: System Environment/Kernel
+ License: GPLv2
+ Version: %{rpmversion}
+@@ -704,6 +707,10 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.8.4-20150505.tar.gz
++patch -sp1 < patches/ccs-patch-3.4-vine-linux-6.diff
++
+ cp %{SOURCE10} Documentation/
+ 
+ # put Vine logo
+@@ -722,6 +729,9 @@
+ for i in *.config
+ do 
+ 	mv $i .config 
++	# TOMOYO Linux
++	cat config.ccs >> .config
++	sed -i -e "s/^CONFIG_DEBUG_INFO=.*/# CONFIG_DEBUG_INFO is not set/" -- .config
+ 	Arch=`head -1 .config | cut -b 3-`
+ 	make ARCH=$Arch oldnoconfig
+ 	echo "# $Arch" > configs/$i
+EOF
+mv kernel34-vl.spec ccs-kernel.spec || die "Can't rename spec file."
+echo ""
+echo ""
+echo ""
+echo "Edit /tmp/ccs-kernel.spec if needed, and run"
+echo "rpmbuild -bb /tmp/ccs-kernel.spec"
+echo "to build kernel rpm packages."
+echo ""
+ARCH=`uname -m`
+echo "I'll start 'rpmbuild -bb --target $ARCH /tmp/ccs-kernel.spec' in 30 seconds. Press Ctrl-C to stop."
+sleep 30
+exec rpmbuild -bb --target $ARCH /tmp/ccs-kernel.spec
+exit 0
