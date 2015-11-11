b36170968fa39948ef3bc1f32cfd5937c68d74ae
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2015-11-11 12:46:15 +0000

    ccs-patch 1.8.5 / ccs-tools 1.8.5

---
+++ 1.8.5/examples/env_chk.c
@@ -0,0 +1,120 @@
+/*
+ * env_chk.c
+ *
+ * An example program for execute_handler .
+ *
+ * Copyright (C) 2005-2011  NTT DATA CORPORATION
+ *
+ * Version: 1.8.5   2015/11/11
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License v2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static void unescape(unsigned char *dest)
+{
+	unsigned char *src = dest;
+	unsigned char c;
+	unsigned char d;
+	unsigned char e;
+	while (1) {
+		c = *src++;
+		if (!c)
+			break;
+		if (c != '\\') {
+			*dest++ = c;
+			continue;
+		}
+		c = *src++;
+		if (c == '\\') {
+			*dest++ = c;
+			continue;
+		}
+		if (c < '0' || c > '3')
+			break;
+		d = *src++;
+		if (d < '0' || d > '7')
+			break;
+		e = *src++;
+		if (e < '0' || e > '7')
+			break;
+		*dest++ = ((c - '0') << 6) + ((d - '0') << 3) + (e - '0');
+	}
+	*dest = '\0';
+}
+
+int main(int raw_argc, char *raw_argv[])
+{
+	int i;
+	int argc;
+	int envc;
+	char *filename;
+	char **argv;
+	char **envp;
+	{ /* Check that I'm an execute handler process.  */
+		int fd = open("/proc/ccs/.execute_handler", 0);
+		close(fd);
+		if (fd == EOF) {
+			fprintf(stderr, "FATAL: I'm not execute_handler.\n");
+			return 1;
+		}
+	}
+	if (raw_argc < 7)
+		return 1;
+	filename = raw_argv[4];
+	argc = atoi(raw_argv[5]);
+	envc = atoi(raw_argv[6]);
+	if (raw_argc != argc + envc + 7)
+		return 1;
+	for (i = 5; i < argc + 5; i++)
+		raw_argv[i] = raw_argv[i + 2];
+	raw_argv[argc + 5] = NULL;
+	for (i = argc + 6; i < argc + envc + 6; i++)
+		raw_argv[i] = raw_argv[i + 1];
+	raw_argv[argc + envc + 6] = NULL;
+	argv = raw_argv + 5;
+	envp = raw_argv + argc + 6;
+	/*
+	 * "/usr/sbin/sshd" executes "/usr/sbin/sshd -R".
+	 * So, don't check environment variables.
+	 */
+	unescape(raw_argv[2]);
+	if (argc == 2 && !strcmp(argv[1], "-R") &&
+	    !strcmp(raw_argv[2], filename)) {
+		execve(filename, argv, envp);
+		return 1;
+	}
+	/*
+	 * Check environment variables passed to execve() request
+	 * and execute the program if it has "CERBERUS=sftp" environment.
+	 */
+	for (i = 0; i < envc; i++) {
+		if (strcmp(envp[i], "CERBERUS=sftp"))
+			continue;
+		while (i < envc) {
+			envp[i] = envp[i + 1];
+			i++;
+		}
+		execve(filename, argv, envp);
+		break;
+	}
+	return 1;
+}
---
+++ 1.8.5/kernel_test/ccs_new_capability_test.c
@@ -0,0 +1,366 @@
+/*
+ * ccs_new_capability_test.c
+ *
+ * Copyright (C) 2005-2012  NTT DATA CORPORATION
+ *
+ * Version: 1.8.5   2015/11/11
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License v2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+#include "include.h"
+
+static const char *capability = "";
+
+static int write_policy(void)
+{
+	FILE *fp = fopen(proc_policy_domain_policy, "r");
+	char buffer[8192];
+	int domain_found = 0;
+	int policy_found = 0;
+	memset(buffer, 0, sizeof(buffer));
+	fprintf(domain_fp, "capability %s\n", capability);
+	fflush(domain_fp);
+	if (!fp) {
+		printf("capability %s : BUG: capability read failed\n",
+		       capability);
+		return 0;
+	}
+	while (fgets(buffer, sizeof(buffer) - 1, fp)) {
+		char *cp = strchr(buffer, '\n');
+		if (cp)
+			*cp = '\0';
+		if (!strncmp(buffer, "<kernel>", 8))
+			domain_found = !strcmp(self_domain, buffer);
+		if (!domain_found)
+			continue;
+		if (!strncmp(buffer, "capability ", 11) &&
+		    !strcmp(buffer + 11, capability)) {
+			policy_found = 1;
+			break;
+		}
+	}
+	fclose(fp);
+	if (!policy_found) {
+		printf("capability %s : BUG: capability write failed\n",
+		       capability);
+		return 0;
+	}
+	errno = 0;
+	return 1;
+}
+
+static void delete_policy(void)
+{
+	fprintf(domain_fp, "delete capability %s\n",
+		capability);
+	fflush(domain_fp);
+}
+
+static void show_result(int result, char should_success)
+{
+	int err = errno;
+	printf("capability %s : ", capability);
+	if (should_success) {
+		if (result != EOF)
+			printf("OK\n");
+		else
+			printf("FAILED: %s\n", strerror(err));
+	} else {
+		if (result == EOF) {
+			if (err == EPERM)
+				printf("OK: Permission denied.\n");
+			else
+				printf("FAILED: %s\n", strerror(err));
+		} else {
+			printf("BUG\n");
+		}
+	}
+}
+
+static void set_capability(void)
+{
+	char buffer[128];
+	memset(buffer, 0, sizeof(buffer));
+	snprintf(buffer, sizeof(buffer) - 1, "capability::%s", capability);
+	set_profile(3, buffer);
+}
+
+static void unset_capability(void)
+{
+	char buffer[128];
+	memset(buffer, 0, sizeof(buffer));
+	snprintf(buffer, sizeof(buffer) - 1, "capability::%s", capability);
+	set_profile(0, buffer);
+}
+
+static void stage_capability_test(void)
+{
+	char tmp1[128];
+	char tmp2[128];
+	int ret_ignored;
+	memset(tmp1, 0, sizeof(tmp1));
+	memset(tmp2, 0, sizeof(tmp2));
+
+	capability = "use_route";
+	set_capability();
+	if (write_policy()) {
+		int fd = socket(AF_ROUTE, SOCK_RAW, 0);
+		show_result(fd, 1);
+		if (fd != EOF)
+			close(fd);
+		delete_policy();
+		fd = socket(AF_ROUTE, SOCK_RAW, 0);
+		show_result(fd, 0);
+		if (fd != EOF)
+			close(fd);
+	}
+	unset_capability();
+
+	capability = "use_packet";
+	set_capability();
+	if (write_policy()) {
+		int fd = socket(AF_PACKET, SOCK_RAW, 0);
+		show_result(fd, 1);
+		if (fd != EOF)
+			close(fd);
+		delete_policy();
+		fd = socket(AF_PACKET, SOCK_RAW, 0);
+		show_result(fd, 0);
+		if (fd != EOF)
+			close(fd);
+	}
+	unset_capability();
+
+	capability = "use_kernel_module";
+	set_capability();
+	if (write_policy()) {
+		if (!is_kernel26)
+			show_result((long) create_module("", 0), 1);
+		show_result(init_module("", NULL), 1);
+		show_result(delete_module(""), 1);
+		delete_policy();
+		if (!is_kernel26)
+			show_result((long) create_module("", 0), 0);
+		show_result(init_module("", NULL), 0);
+		show_result(delete_module(""), 0);
+	}
+	unset_capability();
+
+	capability = "SYS_REBOOT";
+	set_capability();
+	if (write_policy()) {
+		FILE *fp = fopen("/proc/sys/kernel/ctrl-alt-del", "a+");
+		unsigned int c;
+		if (fp && fscanf(fp, "%u", &c) == 1) {
+			show_result(reboot(LINUX_REBOOT_CMD_CAD_ON), 1);
+			delete_policy();
+			show_result(reboot(LINUX_REBOOT_CMD_CAD_ON), 0);
+			rewind(fp);
+			fprintf(fp, "%u\n", c);
+		} else {
+			/* Use invalid value */
+			show_result(reboot(0x0000C0DE), 1);
+			delete_policy();
+			show_result(reboot(0x0000C0DE), 0);
+		}
+		if (fp)
+			fclose(fp);
+	}
+	unset_capability();
+
+	capability = "SYS_KEXEC_LOAD";
+	set_capability();
+	if (write_policy()) {
+#ifdef __NR_sys_kexec_load
+		if (is_kernel26)
+			show_result(sys_kexec_load(0, 0, NULL, 0), 1);
+#endif
+		delete_policy();
+#ifdef __NR_sys_kexec_load
+		if (is_kernel26)
+			show_result(sys_kexec_load(0, 0, NULL, 0), 0);
+#endif
+	}
+	unset_capability();
+
+	capability = "SYS_VHANGUP";
+	set_capability();
+	if (write_policy()) {
+		int status = 0;
+		int pipe_fd[2] = { EOF, EOF };
+		ret_ignored = pipe(pipe_fd);
+		switch (fork()) {
+		case 0:
+			setsid();
+			errno = 0;
+			vhangup();
+			status = errno;
+			ret_ignored = write(pipe_fd[1], &status,
+					    sizeof(status));
+			_exit(0);
+		case -1:
+			fprintf(stderr, "fork() failed.\n");
+			break;
+		default:
+			close(pipe_fd[1]);
+			ret_ignored = read(pipe_fd[0], &status, sizeof(status));
+			wait(NULL);
+			close(pipe_fd[0]);
+			errno = status;
+			show_result(status ? EOF : 0, 1);
+		}
+		delete_policy();
+		status = 0;
+		ret_ignored = pipe(pipe_fd);
+		switch (fork()) {
+		case 0:
+			errno = 0;
+			setsid();
+			vhangup();
+			status = errno;
+			ret_ignored = write(pipe_fd[1], &status,
+					    sizeof(status));
+			_exit(0);
+		case -1:
+			fprintf(stderr, "fork() failed.\n");
+			break;
+		default:
+			close(pipe_fd[1]);
+			ret_ignored = read(pipe_fd[0], &status, sizeof(status));
+			wait(NULL);
+			close(pipe_fd[0]);
+			errno = status;
+			show_result(status ? EOF : 0, 0);
+		}
+	}
+	unset_capability();
+
+	capability = "SYS_TIME";
+	set_capability();
+	if (write_policy()) {
+		struct timeval tv;
+		struct timezone tz;
+		struct timex buf;
+		time_t now = time(NULL);
+		show_result(stime(&now), 1);
+		gettimeofday(&tv, &tz);
+		show_result(settimeofday(&tv, &tz), 1);
+		memset(&buf, 0, sizeof(buf));
+		buf.modes = 0x100; /* Use invalid value so that the clock
+				      won't change. */
+		show_result(adjtimex(&buf), 1);
+		delete_policy();
+		now = time(NULL);
+		show_result(stime(&now), 0);
+		gettimeofday(&tv, &tz);
+		show_result(settimeofday(&tv, &tz), 0);
+		memset(&buf, 0, sizeof(buf));
+		buf.modes = 0x100; /* Use invalid value so that the clock
+				      won't change. */
+		show_result(adjtimex(&buf), 0);
+	}
+	unset_capability();
+
+	capability = "SYS_NICE";
+	set_capability();
+	if (write_policy()) {
+		show_result(nice(0), 1);
+		show_result(setpriority(PRIO_PROCESS, pid,
+					getpriority(PRIO_PROCESS, pid)), 1);
+		delete_policy();
+		show_result(nice(0), 0);
+		show_result(setpriority(PRIO_PROCESS, pid,
+					getpriority(PRIO_PROCESS, pid)), 0);
+	}
+	unset_capability();
+
+	capability = "SYS_SETHOSTNAME";
+	set_capability();
+	if (write_policy()) {
+		char buffer[4096];
+		memset(buffer, 0, sizeof(buffer));
+		gethostname(buffer, sizeof(buffer) - 1);
+		show_result(sethostname(buffer, strlen(buffer)), 1);
+		ret_ignored = getdomainname(buffer, sizeof(buffer) - 1);
+		show_result(setdomainname(buffer, strlen(buffer)), 1);
+		delete_policy();
+		gethostname(buffer, sizeof(buffer) - 1);
+		show_result(sethostname(buffer, strlen(buffer)), 0);
+		ret_ignored = getdomainname(buffer, sizeof(buffer) - 1);
+		show_result(setdomainname(buffer, strlen(buffer)), 0);
+	}
+	unset_capability();
+
+	capability = "SYS_PTRACE";
+	set_capability();
+	if (write_policy()) {
+		int status = 0;
+		int pipe_fd[2] = { EOF, EOF };
+		ret_ignored = pipe(pipe_fd);
+		switch (fork()) {
+		case 0:
+			errno = 0;
+			ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+			status = errno;
+			ret_ignored = write(pipe_fd[1], &status,
+					    sizeof(status));
+			_exit(0);
+		case -1:
+			fprintf(stderr, "fork() failed.\n");
+			break;
+		default:
+			close(pipe_fd[1]);
+			ret_ignored = read(pipe_fd[0], &status, sizeof(status));
+			wait(NULL);
+			close(pipe_fd[0]);
+			errno = status;
+			show_result(status ? EOF : 0, 1);
+		}
+		delete_policy();
+		status = 0;
+		ret_ignored = pipe(pipe_fd);
+		switch (fork()) {
+		case 0:
+			errno = 0;
+			ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+			status = errno;
+			ret_ignored = write(pipe_fd[1], &status,
+					    sizeof(status));
+			_exit(0);
+		case -1:
+			fprintf(stderr, "fork() failed.\n");
+			break;
+		default:
+			close(pipe_fd[1]);
+			ret_ignored = read(pipe_fd[0], &status, sizeof(status));
+			wait(NULL);
+			close(pipe_fd[0]);
+			errno = status;
+			show_result(status ? EOF : 0, 0);
+		}
+	}
+	unset_capability();
+}
+
+int main(int argc, char *argv[])
+{
+	ccs_test_init();
+	stage_capability_test();
+	clear_status();
+	if (0) { /* To suppress "defined but not used" warnings. */
+		write_domain_policy("", 0);
+		write_exception_policy("", 0);
+	}
+	return 0;
+}
