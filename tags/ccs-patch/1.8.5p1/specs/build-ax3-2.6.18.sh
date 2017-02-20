b09b76a8388d05434de9dc76ad93031a3f0496b1
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2017-02-20 13:38:02 +0000

    ccs-patch 1.8.5p1

---
+++ tags/ccs-patch/1.8.5p1/specs/build-ax3-2.6.18.sh
@@ -0,0 +1,92 @@
+#! /bin/sh
+#
+# This is a kernel build script for Asianux 3's 2.6.18 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-2.6.18-398.1.AXS3.src.rpm ]
+then
+    wget http://ftp.miraclelinux.com/pub/Asianux/Server/3.0/updates/src/kernel-2.6.18-398.1.AXS3.src.rpm || die "Can't download source package."
+fi
+LANG=C rpm --checksig kernel-2.6.18-398.1.AXS3.src.rpm | grep -F ': (sha1) dsa sha1 md5 gpg OK' || die "Can't verify signature."
+rpm -ivh kernel-2.6.18-398.1.AXS3.src.rpm || die "Can't install source package."
+
+cd /usr/src/asianux/SOURCES/ || die "Can't chdir to /usr/src/asianux/SOURCES/ ."
+if [ ! -r ccs-patch-1.8.5-20170220.tar.gz ]
+then
+    wget -O ccs-patch-1.8.5-20170220.tar.gz 'http://osdn.jp/frs/redir.php?f=/tomoyo/49684/ccs-patch-1.8.5-20170220.tar.gz' || die "Can't download patch."
+fi
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+cp -p /usr/src/asianux/SPECS/kernel.spec . || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- kernel.spec
++++ kernel.spec
+@@ -68,7 +68,7 @@
+ %define kversion 2.6.%{sublevel}.%{stablerev}
+ %define rpmversion 2.6.%{sublevel}
+ # %dist is defined in Asianux VPBS
+-%define release %{revision}.1%{dist}
++%define release %{revision}.1%{dist}_tomoyo_1.8.5p1
+ %define signmodules 0
+ %define xen_hv_cset 15502
+ %define xen_abi_ver 3.1
+@@ -277,6 +277,9 @@
+ # to versions below the minimum
+ #
+ 
++# TOMOYO Linux
++%define signmodules 0
++
+ #
+ # First the general kernel 2.6 required versions as per
+ # Documentation/Changes
+@@ -307,7 +310,7 @@
+ #
+ %define kernel_prereq  fileutils, module-init-tools, initscripts >= 8.11.1-1, mkinitrd >= 4.2.21-1
+ 
+-Name: kernel
++Name: ccs-kernel
+ Group: System Environment/Kernel
+ License: GPLv2
+ URL: http://www.kernel.org/
+@@ -1042,6 +1045,10 @@
+ %patch200100 -p1
+ # end of Asianux patches
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.8.5-20170220.tar.gz
++patch -sp1 < patches/ccs-patch-2.6.18-asianux-3.diff
++
+ cp %{SOURCE10} Documentation/
+ 
+ mkdir configs
+@@ -1093,6 +1100,8 @@
+ for i in `ls *86*.config`
+ do
+   mv $i .config
++  # TOMOYO Linux
++  cat config.ccs >> .config
+   Arch=`head -1 .config | cut -b 3-`
+   make ARCH=$Arch nonint_oldconfig > /dev/null
+   echo "# $Arch" > configs/$i
+EOF
+mv kernel.spec ccs-kernel.spec || die "Can't rename spec file."
+echo ""
+echo ""
+echo ""
+echo "Edit /tmp/ccs-kernel.spec if needed, and run"
+echo "rpmbuild -bb /tmp/ccs-kernel.spec"
+echo "to build kernel rpm packages."
+echo ""
+ARCH=`uname -m`
+echo "I'll start 'rpmbuild -bb --target $ARCH --with baseonly --without debug --without debuginfo --without source /tmp/ccs-kernel.spec' in 30 seconds. Press Ctrl-C to stop."
+sleep 30
+exec rpmbuild -bb --target $ARCH --with baseonly --without debug --without debuginfo --without source /tmp/ccs-kernel.spec
+exit 0
---
+++ tags/ccs-patch/1.8.5p1/specs/build-f20-3.19.sh
@@ -0,0 +1,110 @@
+#! /bin/sh
+#
+# This is a kernel build script for Fedora 20's 3.19 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+yum -y install tar wget rpm-build make gcc patch redhat-rpm-config xmlto asciidoc gnupg elfutils-devel zlib-devel binutils-devel newt-devel python-devel perl-ExtUtils-Embed pciutils-devel hmaccalc bison net-tools audit-libs-devel pesign bc ncurses-devel
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-3.19.8-100.fc20.src.rpm ]
+then
+    wget https://archives.fedoraproject.org/pub/archive/fedora/linux/updates/20/SRPMS/kernel-3.19.8-100.fc20.src.rpm || die "Can't download source package."
+fi
+LANG=C rpm --checksig kernel-3.19.8-100.fc20.src.rpm | grep -F ': rsa sha1 (md5) pgp md5 OK' || die "Can't verify signature."
+rpm -ivh kernel-3.19.8-100.fc20.src.rpm || die "Can't install source package."
+
+cd ~/rpmbuild/SOURCES/ || die "Can't chdir to ~/rpmbuild/SOURCES/ ."
+if [ ! -r ccs-patch-1.8.5-20170220.tar.gz ]
+then
+    wget -O ccs-patch-1.8.5-20170220.tar.gz 'http://osdn.jp/frs/redir.php?f=/tomoyo/49684/ccs-patch-1.8.5-20170220.tar.gz' || die "Can't download patch."
+fi
+
+cd ~/rpmbuild/SPECS/ || die "Can't chdir to ~/rpmbuild/SPECS/ ."
+cp -p kernel.spec ccs-kernel.spec || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- ccs-kernel.spec
++++ ccs-kernel.spec
+@@ -31,7 +31,7 @@
+ #
+ # (Uncomment the '#' and both spaces below to set the buildid.)
+ #
+-# % define buildid .local
++%define buildid _tomoyo_1.8.5p1
+ ###################################################################
+ 
+ # The buildid can also be specified on the rpmbuild command line
+@@ -477,7 +477,7 @@
+ AutoProv: yes\
+ %{nil}
+ 
+-Name: kernel%{?variant}
++Name: ccs-kernel%{?variant}
+ Group: System Environment/Kernel
+ License: GPLv2 and Redistributable, no modification permitted
+ URL: http://www.kernel.org/
+@@ -964,7 +964,7 @@
+ AutoReqProv: no\
+ Requires(pre): /usr/bin/find\
+ Requires: perl\
+-%description -n kernel%{?variant}%{?1:-%{1}}-devel\
++%description -n ccs-kernel%{?variant}%{?1:-%{1}}-devel\
+ This package provides kernel headers and makefiles sufficient to build modules\
+ against the %{?2:%{2} }kernel package.\
+ %{nil}
+@@ -984,7 +984,7 @@
+ Provides: kernel%{?1:-%{1}}-modules-extra-uname-r = %{KVERREL}%{?1:+%{1}}\
+ Requires: kernel-uname-r = %{KVERREL}%{?1:+%{1}}\
+ AutoReqProv: no\
+-%description -n kernel%{?variant}%{?1:-%{1}}-modules-extra\
++%description -n ccs-kernel%{?variant}%{?1:-%{1}}-modules-extra\
+ This package provides less commonly used kernel modules for the %{?2:%{2} }kernel package.\
+ %{nil}
+ 
+@@ -1527,6 +1527,10 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.8.5-20170220.tar.gz
++patch -sp1 < patches/ccs-patch-3.19-fedora-20.diff
++
+ %endif
+ 
+ # Any further pre-build tree manipulations happen here.
+@@ -1551,6 +1555,17 @@
+ for i in *.config
+ do
+   mv $i .config
++  # TOMOYO Linux 2.5
++  sed -i -e 's/# CONFIG_SECURITY_PATH is not set/CONFIG_SECURITY_PATH=y/' -- .config
++  sed -i -e 's/# CONFIG_SECURITY_TOMOYO is not set/CONFIG_SECURITY_TOMOYO=y/' -- .config
++  echo 'CONFIG_SECURITY_TOMOYO_MAX_ACCEPT_ENTRY=2048' >> .config
++  echo 'CONFIG_SECURITY_TOMOYO_MAX_AUDIT_LOG=1024' >> .config
++  echo '# CONFIG_SECURITY_TOMOYO_OMIT_USERSPACE_LOADER is not set' >> .config
++  echo 'CONFIG_SECURITY_TOMOYO_POLICY_LOADER="/sbin/tomoyo-init"' >> .config
++  echo 'CONFIG_SECURITY_TOMOYO_ACTIVATION_TRIGGER="/sbin/init"' >> .config
++  echo '# CONFIG_DEFAULT_SECURITY_TOMOYO is not set' >> .config
++  # TOMOYO Linux 1.8
++  cat config.ccs >> .config
+   Arch=`head -1 .config | cut -b 3-`
+   make ARCH=$Arch listnewconfig | grep -E '^CONFIG_' >.newoptions || true
+ %if %{listnewconfig_fail}
+EOF
+echo ""
+echo ""
+echo ""
+echo "Edit ~/rpmbuild/SPECS/ccs-kernel.spec if needed, and run"
+echo "rpmbuild -bb ~/rpmbuild/SPECS/ccs-kernel.spec"
+echo "to build kernel rpm packages."
+echo ""
+ARCH=`uname -m`
+echo "I'll start 'rpmbuild -bb --target $ARCH --without debug --without debuginfo ~/rpmbuild/SPECS/ccs-kernel.spec' in 30 seconds. Press Ctrl-C to stop."
+sleep 30
+exec rpmbuild -bb --target $ARCH --without debug --without debuginfo ~/rpmbuild/SPECS/ccs-kernel.spec
+exit 0
