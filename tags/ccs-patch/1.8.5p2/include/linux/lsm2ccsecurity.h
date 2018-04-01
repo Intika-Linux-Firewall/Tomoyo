538a99a6e6ee4cc29b809a44ffdb0edb5eee1042
kumaneko <kumaneko@c82e0f69-1e22-0410-aaae-aa596cd3b9ac>
2018-04-01 10:58:26 +0000

    ccs-patch 1.8.5p2

---
+++ tags/ccs-patch/1.8.5p2/include/linux/lsm2ccsecurity.h
@@ -0,0 +1,216 @@
+/*
+ * include/linux/lsm2ccsecurity.h
+ *
+ * Copyright (C) 2005-2012  NTT DATA CORPORATION
+ *
+ * Version: 1.8.5   2015/11/11
+ */
+
+#ifndef _LINUX_LSM2CCSECURITY_H
+#define _LINUX_LSM2CCSECURITY_H
+
+#include <linux/version.h>
+#include <linux/uidgid.h>
+
+#ifdef CONFIG_CCSECURITY
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0)
+int ccs_settime(const struct timespec64 *ts, const struct timezone *tz);
+#else
+int ccs_settime(const struct timespec *ts, const struct timezone *tz);
+#endif
+int ccs_sb_umount(struct vfsmount *mnt, int flags);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 1, 0)
+int ccs_inode_getattr(struct vfsmount *mnt, struct dentry *dentry);
+#else
+int ccs_inode_getattr(const struct path *path);
+#endif
+int ccs_file_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+int ccs_file_fcntl(struct file *file, unsigned int cmd, unsigned long arg);
+int ccs_file_open(struct file *file, const struct cred *cred);
+int ccs_socket_create(int family, int type, int protocol, int kern);
+int ccs_socket_bind(struct socket *sock, struct sockaddr *address,
+		    int addrlen);
+int ccs_socket_connect(struct socket *sock, struct sockaddr *address,
+		       int addrlen);
+int ccs_socket_listen(struct socket *sock, int backlog);
+int ccs_socket_sendmsg(struct socket *sock, struct msghdr *msg, int size);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0)
+int ccs_path_chmod(const struct path *path, umode_t mode);
+int ccs_path_chown(const struct path *path, kuid_t uid, kgid_t gid);
+int ccs_path_chroot(const struct path *path);
+int ccs_path_link(struct dentry *old_dentry, const struct path *new_dir,
+		  struct dentry *new_dentry);
+int ccs_path_mkdir(const struct path *dir, struct dentry *dentry,
+		   umode_t mode);
+int ccs_path_mknod(const struct path *dir, struct dentry *dentry, umode_t mode,
+		   unsigned int dev);
+int ccs_path_rename(const struct path *old_dir, struct dentry *old_dentry,
+		    const struct path *new_dir, struct dentry *new_dentry);
+int ccs_path_rmdir(const struct path *dir, struct dentry *dentry);
+int ccs_path_symlink(const struct path *dir, struct dentry *dentry,
+		     const char *old_name);
+int ccs_path_truncate(const struct path *path);
+int ccs_path_unlink(const struct path *dir, struct dentry *dentry);
+int ccs_sb_mount(const char *dev_name, const struct path *path,
+		 const char *type, unsigned long flags, void *data);
+int ccs_sb_pivotroot(const struct path *old_path, const struct path *new_path);
+#else
+int ccs_path_chmod(struct path *path, umode_t mode);
+int ccs_path_chown(struct path *path, kuid_t uid, kgid_t gid);
+int ccs_path_chroot(struct path *path);
+int ccs_path_link(struct dentry *old_dentry, struct path *new_dir,
+		  struct dentry *new_dentry);
+int ccs_path_mkdir(struct path *dir, struct dentry *dentry, umode_t mode);
+int ccs_path_mknod(struct path *dir, struct dentry *dentry, umode_t mode,
+		   unsigned int dev);
+int ccs_path_rename(struct path *old_dir, struct dentry *old_dentry,
+		    struct path *new_dir, struct dentry *new_dentry);
+int ccs_path_rmdir(struct path *dir, struct dentry *dentry);
+int ccs_path_symlink(struct path *dir, struct dentry *dentry,
+		     const char *old_name);
+int ccs_path_truncate(struct path *path);
+int ccs_path_unlink(struct path *dir, struct dentry *dentry);
+int ccs_sb_mount(const char *dev_name, struct path *path, const char *type,
+		 unsigned long flags, void *data);
+int ccs_sb_pivotroot(struct path *old_path, struct path *new_path);
+#endif
+
+#else
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0)
+static inline int ccs_settime(const struct timespec64 *ts,
+			      const struct timezone *tz)
+{
+	return 0;
+}
+#else
+static inline int ccs_settime(const struct timespec *ts,
+			      const struct timezone *tz)
+{
+	return 0;
+}
+#endif
+static inline int ccs_sb_mount(const char *dev_name, const struct path *path,
+			       const char *type, unsigned long flags,
+			       void *data)
+{
+	return 0;
+}
+static inline int ccs_sb_umount(struct vfsmount *mnt, int flags)
+{
+	return 0;
+}
+static inline int ccs_sb_pivotroot(const struct path *old_path,
+				   const struct path *new_path)
+{
+	return 0;
+}
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 1, 0)
+static inline int ccs_inode_getattr(struct vfsmount *mnt,
+				    struct dentry *dentry)
+{
+	return 0;
+}
+#else
+static inline int ccs_inode_getattr(const struct path *path)
+{
+	return 0;
+}
+#endif
+static inline int ccs_file_ioctl(struct file *file, unsigned int cmd,
+				 unsigned long arg)
+{
+	return 0;
+}
+static inline int ccs_file_fcntl(struct file *file, unsigned int cmd,
+				 unsigned long arg)
+{
+	return 0;
+}
+static inline int ccs_file_open(struct file *file, const struct cred *cred)
+{
+	return 0;
+}
+static inline int ccs_socket_create(int family, int type, int protocol,
+				    int kern)
+{
+	return 0;
+}
+static inline int ccs_socket_bind(struct socket *sock,
+				  struct sockaddr *address, int addrlen)
+{
+	return 0;
+}
+static inline int ccs_socket_connect(struct socket *sock,
+				     struct sockaddr *address, int addrlen)
+{
+	return 0;
+}
+static inline int ccs_socket_listen(struct socket *sock, int backlog)
+{
+	return 0;
+}
+static inline int ccs_socket_sendmsg(struct socket *sock, struct msghdr *msg,
+				     int size)
+{
+	return 0;
+}
+static inline int ccs_path_unlink(const struct path *dir,
+				  struct dentry *dentry)
+{
+	return 0;
+}
+static inline int ccs_path_mkdir(const struct path *dir, struct dentry *dentry,
+				 umode_t mode)
+{
+	return 0;
+}
+static inline int ccs_path_rmdir(const struct path *dir, struct dentry *dentry)
+{
+	return 0;
+}
+static inline int ccs_path_mknod(const struct path *dir, struct dentry *dentry,
+				 umode_t mode, unsigned int dev)
+{
+	return 0;
+}
+static inline int ccs_path_truncate(const struct path *path)
+{
+	return 0;
+}
+static inline int ccs_path_symlink(const struct path *dir,
+				   struct dentry *dentry, const char *old_name)
+{
+	return 0;
+}
+static inline int ccs_path_link(struct dentry *old_dentry,
+				const struct path *new_dir,
+				struct dentry *new_dentry)
+{
+	return 0;
+}
+static inline int ccs_path_rename(const struct path *old_dir,
+				  struct dentry *old_dentry,
+				  const struct path *new_dir,
+				  struct dentry *new_dentry)
+{
+	return 0;
+}
+static inline int ccs_path_chmod(const struct path *path, umode_t mode)
+{
+	return 0;
+}
+static inline int ccs_path_chown(const struct path *path, kuid_t uid,
+				 kgid_t gid)
+{
+	return 0;
+}
+static inline int ccs_path_chroot(const struct path *path)
+{
+	return 0;
+}
+
+#endif /* defined(CONFIG_CCSECURITY) */
+
+#endif /* !defined(_LINUX_LSM2CCSECURITY_H) */
---
+++ tags/ccs-patch/1.8.5p2/specs/build-vl6-4.4.sh
@@ -0,0 +1,93 @@
+#! /bin/sh
+#
+# This is a kernel build script for VineLinux 6's 4.4 kernel.
+#
+
+die () {
+    echo $1
+    exit 1
+}
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+
+if [ ! -r kernel-4.4.110-2vl6.src.rpm ]
+then
+    wget http://updates.vinelinux.org/Vine-6.5/updates/SRPMS/kernel-4.4.110-2vl6.src.rpm || die "Can't download source package."
+fi
+LANG=C rpm --checksig kernel-4.4.110-2vl6.src.rpm | grep -F ': (sha1) dsa sha1 md5 gpg OK' || die "Can't verify signature."
+rpm -ivh kernel-4.4.110-2vl6.src.rpm || die "Can't install source package."
+
+cd ~/rpm/SOURCES/ || die "Can't chdir to ~/rpm/SOURCES/ ."
+if [ ! -r ccs-patch-1.8.5-20180401.tar.gz ]
+then
+    wget -O ccs-patch-1.8.5-20180401.tar.gz 'http://osdn.jp/frs/redir.php?f=/tomoyo/49684/ccs-patch-1.8.5-20180401.tar.gz' || die "Can't download patch."
+fi
+
+cd /tmp/ || die "Can't chdir to /tmp/ ."
+cp -p ~/rpm/SPECS/kernel44-vl.spec . || die "Can't copy spec file."
+patch << "EOF" || die "Can't patch spec file."
+--- kernel44-vl.spec
++++ kernel44-vl.spec
+@@ -34,7 +34,7 @@
+ %define patchlevel 110
+ %define kversion 4.%{sublevel}
+ %define rpmversion 4.%{sublevel}.%{patchlevel}
+-%define release 2%{?_dist_release}
++%define release 2%{?_dist_release}_tomoyo_1.8.5p2
+ 
+ %define make_target bzImage
+ %define hdrarch %_target_cpu
+@@ -126,6 +126,9 @@
+ # to versions below the minimum
+ #
+ 
++# TOMOYO Linux
++%define signmodules 0
++
+ #
+ # First the general kernel 2.6 required versions as per
+ # Documentation/Changes
+@@ -158,7 +161,7 @@
+ #
+ %define kernel_prereq  fileutils, %{kmod}, initscripts >= 8.80, mkinitrd >= 6.0.93, linux-firmware >= 20110601-1
+ 
+-Name: kernel
++Name: ccs-kernel
+ Group: System Environment/Kernel
+ License: GPLv2
+ Version: %{rpmversion}
+@@ -670,6 +673,10 @@
+ 
+ # END OF PATCH APPLICATIONS
+ 
++# TOMOYO Linux
++tar -zxf %_sourcedir/ccs-patch-1.8.5-20180401.tar.gz
++patch -sp1 < patches/ccs-patch-4.4-vine-linux-6.diff
++
+ cp %{SOURCE10} Documentation/
+ 
+ # put Vine logo
+@@ -688,6 +695,9 @@
+ for i in *.config
+ do 
+ 	mv $i .config 
++	# TOMOYO Linux
++	cat config.ccs >> .config
++	sed -i -e "s/^CONFIG_DEBUG_INFO=.*/# CONFIG_DEBUG_INFO is not set/" -- .config
+ 	Arch=`head -1 .config | cut -b 3-`
+ 	make ARCH=$Arch oldnoconfig
+ 	echo "# $Arch" > configs/$i
+EOF
+mv kernel44-vl.spec ccs-kernel.spec || die "Can't rename spec file."
+echo ""
+echo ""
+echo ""
+echo "Edit /tmp/ccs-kernel.spec if needed, and run"
+echo "rpmbuild -bb /tmp/ccs-kernel.spec"
+echo "to build kernel rpm packages."
+echo ""
+ARCH=`uname -m`
+echo "I'll start 'rpmbuild -bb --target $ARCH /tmp/ccs-kernel.spec' in 30 seconds. Press Ctrl-C to stop."
+sleep 30
+exec rpmbuild -bb --target $ARCH /tmp/ccs-kernel.spec
+exit 0
